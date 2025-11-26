8ab2e46898fa1dbdf1de072466cc4c8a80d8ff5b
Landon Burtle <landonburtle10@gmail.com>
2025-11-25 19:02:04 -0500

    Added functionality for pulling flight data and coordinates speifically

--- src/ingest/producer_opensky.py
+++ src/ingest/producer_opensky.py
@@ -6,22 +6,28 @@ from kafka import KafkaProducer
 import logging
 import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 rest = REST()
-
-# get all states
-# states = rest.states()
-
-# get all states in the last 10 minutes ## Example
-## states_last_10 = rest.states(last=10)
 
 def get_current_states():
     states = rest.states()
     return states
 
-def get_states_last_10_minutes():
-    states_last_10 = rest.states(last=10)
-    return states_last_10
+def get_coordinates(states):
+    return states[["callsign","latitude", "longitude"]].to_dict("records")
 
+def main():
+    while True:
+        states = get_current_states()
+        logger.info(f"Current states: {states}")
+        coords = get_coordinates(states)
+        logger.info(f"Coordinates: {coords}")
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
